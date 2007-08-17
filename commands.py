442ddd95081dee22cc4ac05624e15bad2e9aa408
Julian Phillips <julian@quantumfyre.co.uk>
2007-08-17 01:03:06 +0100

    Add beginnings of get-dir command
    Currently an error is always returned to the client, and no actual
    work is done yet - but the arguments should be parsed correctly.

--- commands.py
+++ commands.py
@@ -41,6 +41,39 @@ def check_path(url_base, repos, args):
     return "%s %s" % (gen.success('( )', gen.string('')),
                       gen.success(type))
 
+@cmd_func('get-dir')
+def get_dir(url_base, repos, args):
+    url = url_base
+    rev = None
+
+    path = parse.string(args.pop(0))
+    if len(path) > 0:
+        url = '/'.join((url_base, path))
+
+    arg = args.pop(0)
+    if isinstance(arg, list):
+        rev = int(arg[0])
+        arg = args.pop(0)
+
+    want_props = parse.bool(arg)
+
+    want_contents = parse.bool(args.pop(0))
+
+    fields = []
+    if len(args) > 0:
+        fields = args[0]
+
+    ref, path = repos.parse_url(url)
+
+    print "ref: %s" % ref
+    print "path: %s" % path
+    print "rev: %s" % rev
+    print "want_props: %s" % want_props
+    print "want_contents: %s" % want_contents
+    print "fields: %s" % fields
+
+    return gen.error(12, 'pah')
+
 def handle_command(url, msg, repos):
     command = msg[0]
     args = msg [1]
