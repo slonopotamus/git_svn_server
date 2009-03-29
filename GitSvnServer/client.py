3ea7a61be81c0f2094d8b3b0d3a81dec267ac663
Julian Phillips <julian@quantumfyre.co.uk>
2009-03-29 18:50:26 +0100

    Sort out the server announce message
    Tidy up the server announce message a bit.  In particular, we might as
    well announce the absent-entries capability - we support the commands
    even if they currently aren't implemented.

--- GitSvnServer/client.py
+++ GitSvnServer/client.py
@@ -4,6 +4,15 @@ import generate as gen
 from repos import find_repos
 from errors import *
 
+server_capabilities = [
+    'edit-pipeline',    # This is required.
+    'svndiff1',         # We support svndiff1
+    'absent-entries',   # We support absent-dir and absent-dir editor commands
+    #'commit-revprops', # We don't currently have _any_ revprop support
+    #'mergeinfo',       # Nope, not yet
+    #'depth',           # Nope, not yet
+]
+
 def parse_client_greeting(msg_str):
     msg = parse.msg(msg_str)
 
@@ -18,9 +27,8 @@ def parse_client_greeting(msg_str):
     return proto_ver, client_caps, url
 
 def connect(link):
-    link.send_msg(gen.success(2, 2,
-                              gen.list('ANONYMOUS'),
-                              gen.list('edit-pipeline', 'svndiff1')))
+    # Send the announce message - we only support protocol version 2.
+    link.send_msg(gen.success(2, 2, gen.list(), gen.list(*server_capabilities)))
 
     client_resp = link.read_msg()
 
