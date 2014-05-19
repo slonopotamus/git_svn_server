25b08e00a7291adc5d441efeb530ecdab8c415af
Marat Radchenko <marat@slonopotamus.org>
2014-05-19 18:19:30 +0400

    get-dir: handle None created-by
    created-by can be None when repository has
    no tags/branches and tags/branches directory
    listing is requested

--- GitSvnServer/commands/get-dir.py
+++ GitSvnServer/commands/get-dir.py
@@ -1,7 +1,7 @@
-
 from GitSvnServer import parse
 from GitSvnServer import generate as gen
 from GitSvnServer.cmd_base import *
+
 
 class GetDir(SimpleCommand):
     _cmd = 'get-dir'
@@ -26,20 +26,25 @@ class GetDir(SimpleCommand):
 
         fields = []
         if len(args) > 0:
-            fields = args[0]
+            fields = args.pop(0)
 
         ls_data = []
         if want_contents:
             for path, kind, size, changed, by, at in repos.ls(url, rev):
                 path_url = "%s/%s" % (url, path)
                 has_props = len(repos.get_props(path_url, rev, False)) == 0
+                if by is None:
+                    by = gen.list()
+                else:
+                    by = gen.list(gen.string(by))
+
                 ls_data.append(gen.list(gen.string(path),
                                         kind,
                                         size,
                                         gen.bool(has_props),
                                         changed,
                                         gen.list(gen.string(at)),
-                                        gen.list(gen.string(by))))
+                                        by))
 
         p = []
         if want_props:
@@ -49,5 +54,3 @@ class GetDir(SimpleCommand):
         response = "%d %s %s" % (rev, gen.list(*p), gen.list(*ls_data))
 
         self.link.send_msg(gen.success(response))
-
-
