fba22c5fa28c083d675bf31023b69a4d984b4146
Marat Radchenko <marat@slonopotamus.org>
2014-05-22 00:31:03 +0400

    editor.py: PEP-8 reformat

--- GitSvnServer/editor.py
+++ GitSvnServer/editor.py
@@ -1,21 +1,24 @@
-
 import parse
 
 from errors import *
 
 edit_cmds = {}
 
+
 def edit_func(name):
     def _edit_func(f):
         edit_cmds.setdefault(name, f)
         return f
+
     return _edit_func
+
 
 @edit_func('target-rev')
 def target_rev(command, args):
     rev = int(args[0])
 
     command.target_rev(rev)
+
 
 @edit_func('open-root')
 def open_root(command, args):
@@ -27,6 +30,7 @@ def open_root(command, args):
 
     command.open_root(rev, root_token)
 
+
 @edit_func('delete-entry')
 def delete_entry(command, args):
     path = parse.string(args[0])
@@ -34,6 +38,7 @@ def delete_entry(command, args):
     dir_token = parse.string(args[2])
 
     command.delete_entry(path, rev, dir_token)
+
 
 @edit_func('add-dir')
 def add_dir(command, args):
@@ -49,6 +54,7 @@ def add_dir(command, args):
 
     command.add_dir(path, parent_token, child_token, copy_path, copy_rev)
 
+
 @edit_func('open-dir')
 def open_dir(command, args):
     path = parse.string(args[0])
@@ -61,6 +67,7 @@ def open_dir(command, args):
 
     command.open_dir(path, parent_token, child_token, rev)
 
+
 @edit_func('change-dir-prop')
 def change_dir_prop(command, args):
     dir_token = parse.string(args[0])
@@ -72,11 +79,13 @@ def change_dir_prop(command, args):
 
     command.change_dir_prop(dir_token, name, value)
 
+
 @edit_func('close-dir')
 def close_dir(command, args):
     dir_token = parse.string(args[0])
 
     command.close_dir(dir_token)
+
 
 @edit_func('absent-dir')
 def absent_dir(command, args):
@@ -84,6 +93,7 @@ def absent_dir(command, args):
     parent_token = parse.string(args[1])
 
     command.absent_dir(path, parent_token)
+
 
 @edit_func('add-file')
 def add_file(command, args):
@@ -99,6 +109,7 @@ def add_file(command, args):
 
     command.add_file(path, dir_token, file_token, copy_path, copy_rev)
 
+
 @edit_func('open-file')
 def open_file(command, args):
     path = parse.string(args[0])
@@ -111,6 +122,7 @@ def open_file(command, args):
 
     command.open_file(path, dir_token, file_token, rev)
 
+
 @edit_func('apply-textdelta')
 def apply_textdelta(command, args):
     file_token = parse.string(args[0])
@@ -121,6 +133,7 @@ def apply_textdelta(command, args):
 
     command.apply_textdelta(file_token, base_checksum)
 
+
 @edit_func('textdelta-chunk')
 def textdelta_chunk(command, args):
     file_token = parse.string(args[0])
@@ -128,11 +141,13 @@ def textdelta_chunk(command, args):
 
     command.textdelta_chunk(file_token, chunk)
 
+
 @edit_func('textdelta-end')
 def textdelta_end(command, args):
     file_token = parse.string(args[0])
 
     command.textdelta_end(file_token)
+
 
 @edit_func('change-file-prop')
 def change_file_prop(command, args):
@@ -145,6 +160,7 @@ def change_file_prop(command, args):
 
     command.change_file_prop(file_token, name, value)
 
+
 @edit_func('close-file')
 def close_file(command, args):
     file_token = parse.string(args[0])
@@ -155,6 +171,7 @@ def close_file(command, args):
 
     command.close_file(file_token, text_checksum)
 
+
 @edit_func('absent-file')
 def absent_file(command, args):
     path = parse.string(args[0])
@@ -162,13 +179,16 @@ def absent_file(command, args):
 
     command.absent_file(path, parent_token)
 
+
 @edit_func('close-edit')
 def finish_edit(command, args):
     command.edit_finish()
 
+
 @edit_func('abort-edit')
 def abort_edit(command, args):
     command.edit_abort()
+
 
 def process(link):
     msg = parse.msg(link.read_msg())
